55b18d7d48ab215294cf06acd34feea161d2674a
naveenchougani <69831778+naveenchougani@users.noreply.github.com>
2025-08-31 01:46:01 +0530

    Replace and Remove a specific character in a string

--- ReplaceAndRemoveChar.java
+++ ReplaceAndRemoveChar.java
@@ -16,10 +16,10 @@ class Main {
         // now to convert it into String which is an object can be used in collect method
         // first convert unicode int into char, then char to String using it valueOf method
         // mapToObj is used to convert primitives to Object
-        String removedUsingStream = str.chars()
-                                       .filter(charInt->charInt!=ch)
-                                       .mapToObj(chi->String.valueOf((char)chi))
-                                       .collect(Collectors.joining());
+        String removedUsingStream = str.chars()                                     // chars() produces unicode (int ) stream of characters
+                                       .filter(charInt->charInt!=ch)               // when both char are being or int and char are being used with operater, int are being compared
+                                       .mapToObj(chi->String.valueOf((char)chi))   // now from unicode->char->String is being converted to Object from an int technically
+                                       .collect(Collectors.joining());             // Collectors.joining() method works with Stream<String>
         System.out.println(removedUsingStream);
 
         // But this is slower when comparedTo mapToObj version i.e, above version
@@ -40,7 +40,7 @@ class Main {
          String replaceusingStreamAndDelimitter=str.chars()
                                      .mapToObj(charInt->charInt==ch?String.valueOf(charToReplacech)
                                                                  :String.valueOf((char)charInt))
-                                     .collect(Collectors.joining(", "));
+                                     .collect(Collectors.joining(", "));                                   // Collectors.joining(", ") with delimitter
         
         System.out.println(replaceusingStreamAndDelimitter);
       

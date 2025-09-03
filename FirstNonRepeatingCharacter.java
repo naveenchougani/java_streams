b9955375cbc8546e6f4b062ffaa4ffd49fc7f85b
naveenchougani <69831778+naveenchougani@users.noreply.github.com>
2025-09-04 00:32:52 +0530

    Create FirstNonRepeatingCharacter.java

---
+++ FirstNonRepeatingCharacter.java
@@ -0,0 +1,17 @@
+import java.util.stream.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        String input = "iindia";
+        Optional<String> non=Stream.of(input.split(""))
+                            .collect(Collectors.groupingBy(s->s,LinkedHashMap::new,Collectors.counting()))
+                            .entrySet().stream().filter(e->e.getValue()==1).map(Map.Entry::getKey).findFirst();
+        
+        non.ifPresent(System.out::println);
+    }
+}

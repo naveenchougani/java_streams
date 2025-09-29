ee9d20fc4203d6c46733f4e5d3acd1e7c01cbd8d
naveenchougani <69831778+naveenchougani@users.noreply.github.com>
2025-09-29 21:34:54 +0530

    using findFirst and map in conjuction

--- FirstNonRepeatingCharacter.java
+++ FirstNonRepeatingCharacter.java
@@ -13,5 +13,12 @@ class Main {
                             .entrySet().stream().filter(e->e.getValue()==1).map(Map.Entry::getKey).findFirst();
         
         non.ifPresent(System.out::println);
+
+        // another version
+        String non=Stream.of(input.split(""))
+                            .collect(Collectors.groupingBy(s->s,LinkedHashMap::new,Collectors.counting()))
+                            .entrySet().stream().filter(e->e.getValue()==1).findFirst().map(Map.Entry::getKey).orElse("not found");
+        
+        System.out.println(non);
     }
 }
